286a2fd7e8b90e9063926b04c62bb920d9ee46c0
farida <farida.hairullina5@gmail.com>
2024-12-02 17:02:36 +0100

    Initial Commit

---
+++ LanguageLearningApp/LanguageLearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageLearningAppApp.swift
+//  LanguageLearningApp
+//
+//  Created by Farida on 02.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LanguageLearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LanguageLearningAppTests/LanguageLearningAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageLearningAppTests.swift
+//  LanguageLearningAppTests
+//
+//  Created by Farida on 02.12.2024.
+//
+
+import Testing
+@testable import LanguageLearningApp
+
+struct LanguageLearningAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
