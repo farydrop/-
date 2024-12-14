9811e06c761cdacbd8d6f61e4f8512c2aff4ee48
farida <farida.hairullina5@gmail.com>
2024-12-14 19:37:41 +0100

    create mvvm

--- LanguageLearningApp/View/AllWordsView.swift
+++ LanguageLearningApp/View/AllWordsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AllWordsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //test new branch
     }
 }
 
